eda0d3fb35d59dbd31259e86d1a164eda4e56c97
Lucas Calheiros <lucaslc@ic.ufal.br>
2018-06-07 01:28:34 -0300

    Remoção da matriz de mecanicas em função de lista de objetos, adicionando atributos a classe mob

--- Game/mecanics.lua
+++ Game/mecanics.lua
@@ -6,73 +6,54 @@ Mec = Object:extend()
 function Mec:new(map) 
 	tamX = map.tamX
 	tamY = map.tamY
-	self.area = {} -- ligação entre mapa e mecanicas
-	self.enemy = {} -- lista de mobs do inimigo
-	self.player = {} -- lista de mobs do player
-	playerCounter = 0
-	enemyCounter = 0
-	for i = 1, tamY+1 do
-		self.area[i] = {}
-		for j = 1, tamX+1 do
+	self.mobs = {} -- lista de mobs
+	mobsCounter = 0
+	for i = 1, tamY do
+		for j = 1, tamX do
 			chance = love.math.random(0,20)
 			if chance > 19 then
-				playerCounter = playerCounter + 1
-				self.player[playerCounter] =  Mob(true)
-				self.area[i][j] = self.player[playerCounter]
+				mobsCounter = mobsCounter + 1
+				self.mobs[mobsCounter] =  Mob(true,i,j)
 			elseif chance < 1 then
-				enemyCounter = enemyCounter + 1
-				self.player[enemyCounter] =  Mob(false)
-				self.area[i][j] =  self.player[enemyCounter]
-			else
-				self.area[i][j] = 0
+				mobsCounter = mobsCounter + 1
+				self.mobs[mobsCounter] =  Mob(false,i,j)
 			end
 		end
 	end
 	self.turn = 0
-	return setmetatable(self, Mec)
 end
 
 function Mec:turn()
 	if sell.turn % 2 == 0 then 
-		self:playerTurn()
+
 	else
-		self:machineTurn()
+
 	end
 end
 
-function Mec:playerTurn()
-	for key, value in ipairs(self.player) do 
-	end
-end
 
-function Mec:machineTurn()
-	for key, value in ipairs(self.enemy) do 
-	end
-end
 
-function Mec:action()
+function Mec:update()
 	
 end
 
 function Mec:info()
-	if self.area[actualTile.x][actualTile.y] ~= 0 then
-		love.graphics.setColor(255, 0, 0)
-		if self.area[actualTile.x][actualTile.y].isPlayer then
-			love.graphics.print("Player", 12, 526)
-		else
-			love.graphics.print("Enemy", 12, 526)
+	for _, mob in ipairs(self.mobs) do
+		if mob.pos.x == actualTile.x and mob.pos.y == actualTile.y then
+			love.graphics.setColor(255, 0, 0)
+			if mob.isPlayer then
+				love.graphics.print("Player", 12, 526)
+			else
+				love.graphics.print("Enemy", 12, 526)
+			end
+			love.graphics.setColor(255, 255, 255)
 		end
-		love.graphics.setColor(255, 255, 255)
 	end
 end
 
 function Mec:draw()
-    for i = camera.y, camera.y + rangeWindow.y do
-        for j = camera.x , camera.x + rangeWindow.x do
-            if self.area[j][i] ~= 0 then
-				self.area[j][i]:print(j-camera.x,i-camera.y)
-            end
-        end
+    for _, mob in ipairs(self.mobs) do
+    	mob:draw(mob.pos.x - camera.x, mob.pos.y - camera.y )
     end
 end
 
--- Game/mob.lua
+++ Game/mob.lua
@@ -2,18 +2,53 @@ Mob = Object:extend()
 
 
 --#TODO
-function Mob:new( isPlayer)
+function Mob:new( isPlayer,x0,y0)
 	self.maxLife = 100
 	self.life = 100
 	self.maxMove = 5
 	self.move = 5
 	self.atk = 30
+	self.isDead = false
 	self.isPlayer = isPlayer --valor booleano
 	self.image = love.graphics.newImage( "mob.png" )
+	self.pos = {
+	x = x0,
+	y = y0
+	}
 end
 
+function Mob:move(moveCost)
+	if moveCost > self.move then
+		return false
+	else
+		self.move = self.move - moveCost
+		return self.move
+	end
+end
+
+function Mob:atack(enemy)
+	enemy.life = enemy.life - self.atk
+end
+
+function Mob:isDead()
+	if self.life <= 0 then
+		self.life = 0
+		self.isDead = true
+	end
+end
+
+function Mob:turnEnd()
+	self.move = self.maxMove
+	self.turn = false
+end
+
+function Mob:newTurn()
+	self.turn = true
+end
+
+
 --#TODO
-function Mob:print(x,y)
+function Mob:draw(x,y)
 	color = self.life*255/100
 	if self.isPlayer then 
 		love.graphics.setColor(0, 0, color)
